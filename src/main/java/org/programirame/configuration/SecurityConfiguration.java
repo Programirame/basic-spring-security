a84e2ccc8a5b2bdb8f65043579a718069a3fb3c4
Igor Stojanovski <igorce@outlook.com>
2017-04-17 22:21:56 +0200

    Initial commit

---
+++ src/main/java/org/programirame/configuration/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package org.programirame.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+}
